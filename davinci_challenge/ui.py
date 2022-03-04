1cafbc759732e26b6e4bfb12c5279a17cd6b5a1a
Dimitri Lezcano <dalezcano19@gmail.com>
2022-03-04 18:21:01 -0500

    Finished triarcs and base for diarcs built

--- davinci_challenge/ui.py
+++ davinci_challenge/ui.py
@@ -75,20 +75,77 @@ class GameApplication:
                 xl = xloc_t - 3 * v * colsep
                 yl = yloc_t + ((i - 24) // 2 * 2 + 3) * heightsep
                 rotation = None
+                # break
 
             elif 30 <= i < 42:  # rotated columns 3.5, 4.5 angled down
-                xl = xloc_t - (v * colsep) // 2 -32
+                xl = xloc_t - (v * colsep) // 2 - 32
                 yl = yloc_t + (i - 30) // 2 * 2 * heightsep - 10
                 rotation = v * 60
 
             elif 42 <= i < 54:  # rotated columns 3.5, 4.5 angled up
                 xl = xloc_t - (v * colsep) // 2 - 32
-                yl = yloc_t + (i - 42) // 2 * 2 * heightsep + th//2 - 10
+                yl = yloc_t + (i - 42) // 2 * 2 * heightsep + th // 2 - 10
+                rotation = -v * 60
+
+            elif 54 <= i < 64:  # rotated columns 2.5, 5.5 angled down
+                xl = xloc_t - 3 * (v * colsep) // 2 - 32
+                yl = yloc_t + ((i - 54) // 2 * 2 * heightsep) + th // 2 - 10
+                rotation = v * 60
+
+
+            elif 64 <= i < 74:  # rotated columns 2.5, 5.5 angled up
+                xl = xloc_t - 3 * (v * colsep) // 2 - 32
+                yl = yloc_t + ((i - 64) // 2 * 2 * heightsep) + 2 * th // 2 - 10
+                rotation = -v * 60
+
+            elif 74 <= i < 82:  # rotated columns 1.5, 6.5 angled down
+                xl = xloc_t - 5 * (v * colsep) // 2 - 32
+                yl = yloc_t + ((i - 74) // 2 * 2 * heightsep) + 2 * th // 2 - 10
+                rotation = v * 60
+
+            elif 82 <= i < 90:  # rotated columns 1.5, 6.5 angled up
+                xl = xloc_t - 5 * (v * colsep) // 2 - 32
+                yl = yloc_t + ((i - 82) // 2 * 2 * heightsep) + 3 * th // 2 - 10
                 rotation = -v * 60
 
             triarc.update( x=xl, y=yl, rotation=rotation )
 
-        # for
+        # for: triarcs
+
+        # diarcs
+        yloc_t += dh // 4
+        lcolsep = dw - 4
+        rcolsep = dw // 4 - 10
+        for i, diarc in enumerate( self.diarcs ):
+            xl, yl, rotation = None, None, None
+            v = 1 if i % 2 == 0 else -1
+            if 0 <= i < 12:  # columns 3 and 4 out
+                xl = xloc_t - (v > 0) * lcolsep + (v < 0) * rcolsep
+                yl = yloc_t + (i - 0) // 2 * 2 * heightsep
+                rotation = -v * 30
+
+            elif 12 <= i < 22:  # columns 3 and 5 in
+                xl = xloc_t - 5 * ((v > 0) * lcolsep - (v < 0) * rcolsep) // 2 + 2 * dw // 3 - v * 2
+                yl = yloc_t + (i - 12) // 2 * 2 * heightsep + th // 2
+                rotation = v * 30
+
+            elif 22 <= i < 32:  # columns 2 and 6 out
+                xl = xloc_t - 9 * ((v > 0) * lcolsep - (v < 0) * rcolsep) // 2 +  3*dw//2
+                yl = yloc_t + (i - 22) // 2 * 2 * heightsep + th//2
+                rotation = -v * 30
+
+            elif 32 <= i < 40:  # columns 2 and 6 in
+                pass
+
+            elif 40 <= i < 48:  # columns 1 and 7 out
+                pass
+
+            elif 48 <= i < 54:  # columns 1 and 7 in
+                pass
+
+            diarc.update( x=xl, y=yl, rotation=rotation, player=2 )
+
+        # for: diarcs
 
     # buildBoard
 
